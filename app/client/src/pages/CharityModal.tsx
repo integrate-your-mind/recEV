19e1e1b117346dd88cb18f3354bccd119078394b
klxu03 Laptop <48634131+klxu03@users.noreply.github.com>
2020-04-04 14:28:06 -0400

    Another one

--- app/client/src/pages/CharityModal.tsx
+++ app/client/src/pages/CharityModal.tsx
@@ -2,9 +2,19 @@ import React from 'react';
 
 import './CharityModal.css';
 
+import { ActionSheetButton } from '@ionic/core';
+import { IonActionSheet, IonChip, IonIcon, IonHeader, IonLabel, IonToolbar, IonButtons, IonContent, IonButton, IonBackButton, IonPage } from '@ionic/react'
+import { callOutline, callSharp, logoTwitter, logoGithub, logoInstagram, shareOutline, shareSharp } from 'ionicons/icons';
+
 const CharityModal: React.FC = props => {
     return (
-        
+        <IonPage>
+            <IonContent>
+                <IonHeader>
+                    
+                </IonHeader>
+            </IonContent>>
+        </IonPage>
     );
 }
 
