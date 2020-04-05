dc84acd719625c74bc8e45e9eabc7e5d93b986e4
klxu03 Laptop <48634131+klxu03@users.noreply.github.com>
2020-04-04 21:46:44 -0400

    Good Donate now, no lazyloading tho

--- app/client/src/pages/Donate.tsx
+++ app/client/src/pages/Donate.tsx
@@ -1,11 +1,95 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-// import ExploreContainer from '../components/ExploreContainer';
 import './Donate.css';
 import CharityItem from '../components/charity-item/charity-item.component';
 
+import Charity from '../models/business/charity';
+
 const Donate = () => {
-    const redCrossTags: string[] = ['Food', 'Clothes', 'Spidey'];
+    const [organizations, setOrganizations] = useState<Array<Charity>>([]);
+
+    // charityName: string;
+    // tags: Array<string>;
+    // charityID: number;
+    // charityImageURL: string;
+    // charityShortDescription: string;
+    // charityLongDescription: string;
+
+    useEffect(() => {
+        setOrganizations([{
+            charityName: 'Red Cross',
+            tags: ['Food', 'Clothes', 'Spidey'],
+            charityID: 1,
+            charityImageURL: 'helpTheRedCross.jpg',
+            charityShortDescription: 'This is the red cross, you know it',
+            charityLongDescription: 'This is also the redcross, you better know it lmao'
+        },
+        {
+            charityName: 'Roman',
+            tags: ['Weapon', 'Crossbow', 'Spidey'],
+            charityID: 2,
+            charityImageURL: 'helpTheRedCross.jpg',
+            charityShortDescription: 'Roman, proof that it works',
+            charityLongDescription: 'This is also the redcross, you better know it lmao'
+        },
+        {
+            charityName: 'Roman',
+            tags: ['Weapon', 'Crossbow', 'Spidey'],
+            charityID: 2,
+            charityImageURL: 'helpTheRedCross.jpg',
+            charityShortDescription: 'Roman, proof that it works',
+            charityLongDescription: 'This is also the redcross, you better know it lmao'
+        },
+        {
+            charityName: 'Roman',
+            tags: ['Weapon', 'Crossbow', 'Spidey'],
+            charityID: 2,
+            charityImageURL: 'helpTheRedCross.jpg',
+            charityShortDescription: 'Roman, proof that it works',
+            charityLongDescription: 'This is also the redcross, you better know it lmao'
+        },
+        {
+            charityName: 'Roman',
+            tags: ['Weapon', 'Crossbow', 'Spidey'],
+            charityID: 2,
+            charityImageURL: 'helpTheRedCross.jpg',
+            charityShortDescription: 'Roman, proof that it works',
+            charityLongDescription: 'This is also the redcross, you better know it lmao'
+        },
+        {
+            charityName: 'Roman',
+            tags: ['Weapon', 'Crossbow', 'Spidey'],
+            charityID: 2,
+            charityImageURL: 'helpTheRedCross.jpg',
+            charityShortDescription: 'Roman, proof that it works',
+            charityLongDescription: 'This is also the redcross, you better know it lmao'
+        },
+        {
+            charityName: 'Roman',
+            tags: ['Weapon', 'Crossbow', 'Spidey'],
+            charityID: 2,
+            charityImageURL: 'helpTheRedCross.jpg',
+            charityShortDescription: 'Roman, proof that it works',
+            charityLongDescription: 'This is also the redcross, you better know it lmao'
+        },
+        {
+            charityName: 'Roman',
+            tags: ['Weapon', 'Crossbow', 'Spidey'],
+            charityID: 2,
+            charityImageURL: 'helpTheRedCross.jpg',
+            charityShortDescription: 'Roman, proof that it works',
+            charityLongDescription: 'This is also the redcross, you better know it lmao'
+        },
+        {
+            charityName: 'Roman',
+            tags: ['Weapon', 'Crossbow', 'Spidey'],
+            charityID: 2,
+            charityImageURL: 'helpTheRedCross.jpg',
+            charityShortDescription: 'Roman, proof that it works',
+            charityLongDescription: 'This is also the redcross, you better know it lmao'
+        }
+    ])
+    }, []);
 
     return (
         <IonPage>
@@ -20,8 +104,10 @@ const Donate = () => {
                         <IonTitle size="large">Donate</IonTitle>
                     </IonToolbar>
                 </IonHeader>
-                {/* <ExploreContainer name="Tab 2 page" /> */}
-                <CharityItem charityName="Red Cross" tags={redCrossTags} charityID={1} />
+                {organizations.map(organization => 
+                    <CharityItem charityName = {organization.charityName} tags = {organization.tags} charityID = {organization.charityID} charityShortDescription = {organization.charityShortDescription}/>
+                )}
+                {/* <CharityItem charityName="Red Cross" tags={redCrossTags} charityID={1} /> */}
 
             </IonContent>
         </IonPage>
