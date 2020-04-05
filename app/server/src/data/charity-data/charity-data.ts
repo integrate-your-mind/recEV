89527d0736f5a4f1a035e253f4fafa54952f9c40
Roman Mondello <rflaiaco@bhcc.edu>
2020-04-05 00:28:01 -0400

    added charity and country models for type-gql

---
+++ app/server/src/data/charity-data/charity-data.ts
@@ -0,0 +1,19 @@
+export interface Charity {
+    charityLegalName: string;
+    imageURL: string;
+    smallDescription: string;
+    longDescription: string;
+    addressLine1: string;
+    townCity: string;
+    state: string;
+    homelessness: string;
+    health: string;
+    country: string;
+    charityWebsite: string;
+    education: boolean;
+    chronicIllness: boolean;
+    disabilities: boolean;
+    unemployment: boolean;
+    veterans: boolean;
+    postcode: number;
+}
--- app/server/src/models/charity.interface.ts
+++ app/server/src/models/charity.interface.ts
@@ -2,12 +2,12 @@ import { Field, InterfaceType, ID } from 'type-graphql';
 
 @InterfaceType()
 export abstract class Charity {
-    @Field(type => ID)
+    @Field((type) => ID)
     id: string;
 
     @Field()
     charityLegalName: string;
-    
+
     @Field({ nullable: true })
     imageURL?: string;
 
@@ -27,28 +27,28 @@ export abstract class Charity {
     state: string;
 
     @Field()
-    postcode: number;
-
-    @Field()
     country: string;
 
     @Field()
     charityWebsite: string;
 
     @Field({ nullable: true })
-    education?: boolean;
+    health?: string;
 
     @Field({ nullable: true })
-    health?: string;
+    homelessness?: string;
+
+    @Field()
+    postcode: number;
+
+    @Field({ nullable: true })
+    education?: boolean;
 
     @Field({ nullable: true })
     chronicIllness?: boolean;
 
     @Field({ nullable: true })
     disabilities?: boolean;
-
-    @Field({ nullable: true })
-    homelessness?: string;
 
     @Field({ nullable: true })
     unemployment?: boolean;
