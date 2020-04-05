18eac747165048498b44731b89c2ed45e5b2f176
klxu03 Laptop <48634131+klxu03@users.noreply.github.com>
2020-04-04 22:44:20 -0400

    Here is the final stuff

--- app/client/src/pages/Map.tsx
+++ app/client/src/pages/Map.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 // import ExploreContainer from '../components/ExploreContainer';
-import './Map.css';
+import './Map.scss';
+
+import mapImage from './img/google-map.jpg'
 
 const Tab3: React.FC = () => {
+
     return (
         <IonPage>
             <IonHeader>
@@ -17,7 +20,7 @@ const Tab3: React.FC = () => {
                         <IonTitle size="large">Tab 3</IonTitle>
                     </IonToolbar>
                 </IonHeader>
-                {/* <ExploreContainer name="Tab 3 page" /> */}
+                <img src={mapImage} />
             </IonContent>
         </IonPage>
     );
