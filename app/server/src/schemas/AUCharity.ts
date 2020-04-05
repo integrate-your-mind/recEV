4f81d0162f6642ca461dd9b593daa213671b0c66
Roman Mondello <rflaiaco@bhcc.edu>
2020-04-05 12:05:21 -0400

    Brain is bursting

---
+++ app/server/src/schemas/AUCharity.ts
@@ -0,0 +1,32 @@
+import { Int, Field, ObjectType } from 'type-graphql';
+import { Charity } from './Charity';
+
+@ObjectType()
+export default class AUCharity extends Charity {
+    @Field((type) => Int)
+    abn!: number;
+
+    @Field()
+    territoryNSW!: boolean;
+
+    @Field()
+    territoryACT!: boolean;
+
+    @Field()
+    territoryNT!: boolean;
+
+    @Field()
+    territoryQLD!: boolean;
+
+    @Field()
+    territorySA!: boolean;
+
+    @Field()
+    territoryTAS!: boolean;
+
+    @Field()
+    territoryVIC!: boolean;
+
+    @Field()
+    territoryWA!: boolean;
+}
--- app/server/src/server.ts
+++ app/server/src/server.ts
@@ -1,2 +1,19 @@
+import { GraphQLServer } from 'graphql-yoga';
 import 'reflect-metadata';
-console.log("hi"); 
+import { buildSchema } from 'type-graphql';
+import CharityResolver from './resolvers/CharityResolver';
+
+async function bootstrap() {
+    const schema = await buildSchema({
+        resolvers: [CharityResolver],
+        emitSchemaFile: true,
+    });
+
+    const server = new GraphQLServer({
+        schema,
+    });
+
+    server.start(() => console.log('Server is running on http://localhost:4000'));
+}
+
+bootstrap();
