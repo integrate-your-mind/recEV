9aca9ca29eda663a4e8d62f9da1fac12698bf330
Roman Mondello <rflaiaco@bhcc.edu>
2020-04-06 19:46:24 -0400

    Added lodash w/tree-shaking, in process of configuring webpack to properly include mutliple .env files in build w/ts

--- app/server/src/index.ts
+++ app/server/src/index.ts
@@ -1,18 +1,26 @@
 /* eslint no-use-before-define: 0 */ // --> OFF
-import { GraphQLServer } from 'graphql-yoga';
 import 'reflect-metadata';
+import './lib/config';
 import { buildSchema } from 'type-graphql';
+import { GraphQLServer } from 'graphql-yoga';
+import { connect } from 'mongoose';
+
 import CharityResolver from './resolvers/CharityResolver';
 
 async function bootstrap(): Promise<void> {
-  const schema = await buildSchema({
-    resolvers: [CharityResolver],
-    emitSchemaFile: true,
-  });
+  try {
+    const mongoose: typeof import('mongoose') = await connect(process.env.MONGO_DB as string); //Implementing type assertion for environment variables is important or else ts with throw an error
+    const schema = await buildSchema({
+      resolvers: [CharityResolver],
+      emitSchemaFile: true,
+    });
 
-  const server = new GraphQLServer({ schema });
+    const server = new GraphQLServer({ schema });
 
-  server.start(() => console.log('Server is running on http://localhost:4000'));
+    server.start(() => console.log('Server is running on http://localhost:4000'));
+  } catch (err) {
+    return Promise.reject(new Error(err));
+  }
 }
 
 bootstrap();
