8ed280965cc0118cc0ec5cb564833d3b1d4777d8
Roman Mondello <rflaiaco@bhcc.edu>
2020-04-07 01:46:02 -0400

    Configued .env, configured docker/docker-compose

--- app/server/src/index.ts
+++ app/server/src/index.ts
@@ -1,6 +1,7 @@
 /* eslint no-use-before-define: 0 */ // --> OFF
 import 'reflect-metadata';
-import './lib/config';
+import './lib/env';
+// import errorReporter from './lib/errors';
 import { buildSchema } from 'type-graphql';
 import { GraphQLServer } from 'graphql-yoga';
 import { connect } from 'mongoose';
@@ -9,12 +10,15 @@ import CharityResolver from './resolvers/CharityResolver';
 
 async function bootstrap(): Promise<void> {
   try {
-    const mongoose: typeof import('mongoose') = await connect(process.env.MONGO_DB as string); //Implementing type assertion for environment variables is important or else ts with throw an error
     const schema = await buildSchema({
       resolvers: [CharityResolver],
       emitSchemaFile: true,
     });
 
+    const mongoose: typeof import('mongoose') = await connect(encodeURI(process.env.MONGO_DB as string), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }); //Implementing type assertion for environment variables is important or else ts with throw an error
     const server = new GraphQLServer({ schema });
 
     server.start(() => console.log('Server is running on http://localhost:4000'));
