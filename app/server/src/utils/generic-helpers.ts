f89373ee0e9bf53cba8c94bad697b4bbc7a002e1
Roman Mondello <rflaiaco@bhcc.edu>
2020-04-08 15:47:09 -0400

    Set up dynamic object in generic helpers 'processData' function'

--- app/server/src/utils/generic-helpers.ts
+++ app/server/src/utils/generic-helpers.ts
@@ -1,7 +1,7 @@
 import each from 'lodash/each';
 
-//Value interface
-//Type interface
+//Value interface *
+//Type interface *
 //instantiate object of type Type
 //use string interpolation to dynamically add the key values to the new object
 
@@ -9,23 +9,26 @@ export interface IValue {
   prop: string;
 }
 
+export interface IType {
+  [name: string]: IValue;
+}
+
 export function processData<T>(data: T[], props: string[]): T[] {
+  const dynamicObject: IType = {}; //t['field1'] = { prop: null };
+
   const processedData = each(data, (dataObject) => {
     each(Object.keys(dataObject), (key) => {
       if (props.includes(key)) {
-        /** Ignore this error */
-        // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        //@ts-ignore
-        console.log('Key Found: \n' + 'Key: [' + key + ']' + '\nValue:' + '[' + dataObject[key.toString()] + ']');
+        dynamicObject[`${key}`] = ((dataObject as unknown) as { [key: string]: unknown })[`${key}`] as IValue;
+        console.log(dynamicObject);
       } else {
-        console.log(
-          /** Ignore this error */
-          // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          //@ts-ignore
-          'Other Value Found: \n' + 'Key: [' + key + ']' + '\nValue:' + '[' + dataObject[key.toString()] + ']',
-        );
+        // console.log(
+        //   /** Ignore this error */
+        //   // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+        //   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        //   //@ts-ignore
+        //   'Other Value Found: \n' + 'Key: [' + key + ']' + '\nValue:' + '[' + dataObject[key.toString()] + ']',
+        // );
       }
     });
   });
