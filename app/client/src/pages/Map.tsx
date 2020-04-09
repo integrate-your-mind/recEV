b41c3d901cf55201d8ffa3f67cfee30d7b632c1f
Roman Mondello <rflaiaco@bhcc.edu>
2020-04-09 19:03:02 -0400

    Fixed merge conflict

--- app/client/src/pages/Map.tsx
+++ app/client/src/pages/Map.tsx
@@ -3,29 +3,9 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from 
 // import ExploreContainer from '../components/ExploreContainer';
 import './Map.scss';
 
-import mapImage from './img/google-map.jpg'
+import mapImage from './img/google-map.jpg';
 
 const Tab3: React.FC = () => {
-<<<<<<< HEAD
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Map</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        {/* <ExploreContainer name="Tab 3 page" /> */}
-      </IonContent>
-    </IonPage>
-  );
-=======
-
     return (
         <IonPage>
             <IonHeader>
@@ -40,11 +20,12 @@ const Tab3: React.FC = () => {
                     </IonToolbar>
                 </IonHeader>
                 <img src={mapImage} />
-                <IonButton color="primary" href="/profile">Profile</IonButton>
+                <IonButton color="primary" href="/profile">
+                    Profile
+                </IonButton>
             </IonContent>
         </IonPage>
     );
->>>>>>> feature
 };
 
 export default Tab3;
