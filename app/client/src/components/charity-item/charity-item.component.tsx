5394d959fc5bbbaa2db6873d85af4a3f00cbdf29
Roman Mondello <rflaiaco@bhcc.edu>
2020-04-10 23:58:55 -0400

    Added lint and fixed all linting problems

--- app/client/src/components/charity-item/charity-item.component.tsx
+++ app/client/src/components/charity-item/charity-item.component.tsx
@@ -5,51 +5,49 @@ import { IonCard, IonCardHeader, IonItem, IonLabel, IonAvatar, IonCardContent, I
 import './charity-item.styles.scss';
 
 const CharityItem: React.FC<{
-    charityName: string;
-    tags: Array<string>;
-    charityID: number;
-    charityShortDescription: string;
+  charityName: string;
+  tags: Array<string>;
+  charityID: number;
+  charityShortDescription: string;
 }> = (props) => {
-    const imageFolder = '../images';
+  const imageFolder = '../images';
 
-    const renderTags = () => {
-        let listOfTags = '';
+  const renderTags = () => {
+    let listOfTags = '';
 
-        for (const tag of props.tags) {
-            listOfTags = listOfTags + tag + ', ';
-        }
+    for (const tag of props.tags) {
+      listOfTags = listOfTags + tag + ', ';
+    }
 
-        return listOfTags.substring(0, listOfTags.length - 2);
-    };
+    return listOfTags.substring(0, listOfTags.length - 2);
+  };
 
-    return (
-        <IonCard button routerLink={`/charity/${props.charityID}`}>
-            <IonCardHeader>
-                {/* <IonItem button detail={false} lines="none" className="speaker-item" routerLink={`/tabs/speakers/${speaker.id}`}> */}
-                <IonItem>
-                    <IonAvatar slot="start">
-                        <img src={`${imageFolder}/kevinxu.jpg`} />
-                    </IonAvatar>
-                    <IonLabel>
-                        <h2>{props.charityName}</h2>
-                        <p>{renderTags()}</p>
-                    </IonLabel>
-                </IonItem>
-            </IonCardHeader>
+  return (
+    <IonCard button routerLink={`/charity/${props.charityID}`}>
+      <IonCardHeader>
+        {/* <IonItem button detail={false} lines="none" className="speaker-item" routerLink={`/tabs/speakers/${speaker.id}`}> */}
+        <IonItem>
+          <IonAvatar slot="start">
+            <img src={`${imageFolder}/kevinxu.jpg`} />
+          </IonAvatar>
+          <IonLabel>
+            <h2>{props.charityName}</h2>
+            <p>{renderTags()}</p>
+          </IonLabel>
+        </IonItem>
+      </IonCardHeader>
 
-            <IonCardContent>
-                <IonList lines="none">
-                    <IonItem>
-                        <IonLabel>
-                            <h3>
-                                {props.charityShortDescription}
-                            </h3>
-                        </IonLabel>
-                    </IonItem>
-                </IonList>
-            </IonCardContent>
-        </IonCard>
-    );
+      <IonCardContent>
+        <IonList lines="none">
+          <IonItem>
+            <IonLabel>
+              <h3>{props.charityShortDescription}</h3>
+            </IonLabel>
+          </IonItem>
+        </IonList>
+      </IonCardContent>
+    </IonCard>
+  );
 };
 
 export default CharityItem;
